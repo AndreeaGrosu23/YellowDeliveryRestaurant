bde636eda54399f7e843ec1c190dabb4019436b0
AndreeaGrosu23 <andreea.grosu87@gmail.com>
2021-02-02 21:27:44 +0200

    Add unit test for ShoppingCartException

--- RestaurantApp/Restaurant/src/test/java/com/codecool/restaurant/PaymentTest.java
+++ RestaurantApp/Restaurant/src/test/java/com/codecool/restaurant/PaymentTest.java
@@ -1,41 +1,46 @@
-//package com.codecool.restaurant;
-//
-//import com.codecool.restaurant.meal.MealsToCartService;
-//import com.codecool.restaurant.payment.PaymentDetailsModel;
-//import com.codecool.restaurant.payment.PaymentService;
-//import com.codecool.restaurant.shoppingCart.ShoppingCartService;
-//import com.codecool.restaurant.user.UserService;
-//import com.codecool.restaurant.order.UserOrderRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.web.client.RestTemplate;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//public class PaymentTest {
-//
-//    @Test
-//    void testRegisterOrderThrowsExceptionWithEmptyCard() {
-//
-//        MealsToCartService mealsToCartService=mock(MealsToCartService.class);
-//
-//        UserService userService=mock(UserService.class);
-//
-//        ShoppingCartService shoppingCartService=mock(ShoppingCartService.class);
-//
-//        UserOrderRepository userOrderRepository=mock(UserOrderRepository.class);
-//
-//        RestTemplate restTemplate=mock(RestTemplate.class);
-//
-//        //mock all services and put them in the constructor + when and then for their methods
-//        PaymentService paymentService = new PaymentService(mealsToCartService, userService, shoppingCartService, userOrderRepository, restTemplate);
-//        PaymentDetailsModel paymentDetailsModel = new PaymentDetailsModel("Wrong username", "success");
-//
-//        when(userService.getUserByUsername("Wrong username")).thenReturn(null);
-//        when(shoppingCartService.getCartByUser(null)).thenReturn(null);
-//
-//        assertThrows(NullPointerException.class, () -> paymentService.registerOrder(paymentDetailsModel));
-//    }
-//}
-//
+package com.codecool.restaurant;
+
+import com.codecool.restaurant.exception.ShoppingCartException;
+import com.codecool.restaurant.meal.MealsToCartService;
+import com.codecool.restaurant.payment.PaymentDetailsModel;
+import com.codecool.restaurant.payment.PaymentService;
+import com.codecool.restaurant.shoppingCart.ShoppingCartService;
+import com.codecool.restaurant.user.User;
+import com.codecool.restaurant.user.UserService;
+import com.codecool.restaurant.order.UserOrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class PaymentTest {
+
+    @Test
+    void testRegisterOrderThrowsExceptionWithEmptyCard() {
+
+        MealsToCartService mealsToCartService=mock(MealsToCartService.class);
+
+        UserService userService=mock(UserService.class);
+
+        ShoppingCartService shoppingCartService=mock(ShoppingCartService.class);
+
+        UserOrderRepository userOrderRepository=mock(UserOrderRepository.class);
+
+        RestTemplate restTemplate=mock(RestTemplate.class);
+
+        //mock all services and put them in the constructor + when and then for their methods
+        PaymentService paymentService = new PaymentService(mealsToCartService, userService, shoppingCartService, userOrderRepository, restTemplate);
+        PaymentDetailsModel paymentDetailsModel = new PaymentDetailsModel("j.doe", "success");
+
+        User user = new User("Jane", "Doe", "j.doe", "j.doe@gmail.com", "", "", "pass");
+        when(userService.getUserByUsername("j.doe")).thenReturn(user);
+        when(shoppingCartService.getCartByUser(user)).thenThrow(new ShoppingCartException());
+
+        assertThrows(ShoppingCartException.class, () -> paymentService.registerOrder(paymentDetailsModel));
+    }
+}
+
