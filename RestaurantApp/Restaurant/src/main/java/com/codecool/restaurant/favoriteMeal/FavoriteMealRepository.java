c70a0926b5144a1b2fd95f848bbcb769d1328ac2
AndreeaGrosu23 <andreea.grosu87@gmail.com>
2021-03-02 14:57:52 +0200

    Refactor favorites feature

--- RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/favoriteMeal/FavoriteMealRepository.java
+++ RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/favoriteMeal/FavoriteMealRepository.java
@@ -2,13 +2,10 @@ package com.codecool.restaurant.favoriteMeal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
+
 
 public interface FavoriteMealRepository extends JpaRepository<FavoriteMeal, Long> {
 
-    List<FavoriteMeal> getAllFavoriteMealsByUserId(Long user_id);
-
-    Optional<FavoriteMeal> findFavoriteMealByIdMeal(String idMeal);
-
+    FavoriteMeal findFavoriteMealByIdMeal(String idMeal);
 }
--- RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/favoriteMeal/FavoriteMealService.java
+++ RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/favoriteMeal/FavoriteMealService.java
@@ -1,31 +1,52 @@
 package com.codecool.restaurant.favoriteMeal;
 
-import com.codecool.restaurant.exception.NoDataFoundException;
+import com.codecool.restaurant.user.User;
+import com.codecool.restaurant.user.UserRepository;
+import com.codecool.restaurant.user.UserService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.Set;
 
 @Service
 public class FavoriteMealService {
 
     private final FavoriteMealRepository favoriteMealRepository;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
-    public FavoriteMealService(FavoriteMealRepository favoriteMealRepository){
+    public FavoriteMealService(FavoriteMealRepository favoriteMealRepository, UserService userService, UserRepository userRepository) {
         this.favoriteMealRepository = favoriteMealRepository;
-    }
-
-    public void addFavoriteMeal(FavoriteMeal favoriteMeal) {
-        favoriteMealRepository.save(favoriteMeal);
-    }
-
-    public List<FavoriteMeal> getAllFavoriteMeals(Long user_id) {
-        return favoriteMealRepository.getAllFavoriteMealsByUserId(user_id);
+        this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @Transactional
-    public void deleteFavoriteMealByIdMeal(String idMeal) {
-        FavoriteMeal favoriteMeal = favoriteMealRepository.findFavoriteMealByIdMeal(idMeal).orElseThrow(NoDataFoundException::new);
-        favoriteMealRepository.delete(favoriteMeal);
+    public void addFavoriteMeal(FavoriteMealDto favoriteMealDto, String username) {
+        User user = userService.getUserByUsername(username);
+
+        if(favoriteMealRepository.findFavoriteMealByIdMeal(favoriteMealDto.getIdMeal())==null) {
+            FavoriteMeal favoriteMeal = new FavoriteMeal(favoriteMealDto.getName(), favoriteMealDto.getImage(), favoriteMealDto.getIdMeal());
+            favoriteMealRepository.save(favoriteMeal);
+            user.getFavoriteMeals().add(favoriteMeal);
+        } else {
+            FavoriteMeal favoriteMeal= favoriteMealRepository.findFavoriteMealByIdMeal(favoriteMealDto.getIdMeal());
+            user.getFavoriteMeals().add(favoriteMeal);
+        }
+
+        userRepository.save(user);
+    }
+
+    public Set<FavoriteMeal> getAllFavoriteMeals(String username) {
+        User user = userService.getUserByUsername(username);
+        return user.getFavoriteMeals();
+    }
+
+    @Transactional
+    public void deleteFavoriteMealByIdMeal(String username, String idMeal) {
+        User user = userService.getUserByUsername(username);
+        FavoriteMeal favoriteMeal = favoriteMealRepository.findFavoriteMealByIdMeal(idMeal);
+        user.getFavoriteMeals().remove(favoriteMeal);
+        userRepository.save(user);
     }
 }
