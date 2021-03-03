4ae55193137b98ff998458b0bb2048aa0663d503
AndreeaGrosu23 <andreea.grosu87@gmail.com>
2021-03-03 17:32:05 +0200

    Refactor swagger

--- RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/RestaurantApplication.java
+++ RestaurantApp/Restaurant/src/main/java/com/codecool/restaurant/RestaurantApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 
 @SpringBootApplication
@@ -28,6 +33,10 @@ public class RestaurantApplication {
 
     @Bean
     public Docket restaurantApp() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
     }
 }
